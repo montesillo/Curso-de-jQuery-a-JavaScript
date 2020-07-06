2d3eab3b028114d38faf073f091a5fdbceb6203b
Hiram Montesillo <montesillo25@gmail.com>
2020-07-06 07:37:09 -0500

    selectores y funciones asincronas

--- src/js/home.js
+++ src/js/home.js
@@ -37,3 +37,59 @@ const getUser = new Promise(function(todoBien, todoMal) {
   .catch(function(message){
     console.log(message)
   })
+
+  // $.ajax('https://randomuser.me/api', {
+  //   method: 'GET',
+  //   success: function(data){
+  //     console.log(data)
+  //   },
+  //   error: function(error){
+  //     console.log(error);
+  //   }
+  // })
+
+fetch('https://randomuser.me/api')
+  .then(function(response){
+    console.log(response)
+    return response.json()
+  })
+  .then(function(user){
+    console.log('user', user.results[0].name.first)
+  })
+  .catch(function(){
+    console.log('algo fallo')
+  });
+
+(async function load(){
+  // await
+  // action
+  //terror
+  // animation
+  async function getData(url){
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+  }
+  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
+  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
+  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+  console.log(actionList, dramaList, animationList);
+
+  const $actionContainer = document.querySelector('#action')
+  const $dramaContainer = document.getElementById('#drama')
+  const $animationContainer = document.getElementById('#animation')
+
+  const $featuringContainer = document.getElementById('#featuring')
+  const $form = document.getElementById('#form')
+  const $home = document.getElementById('#home')
+  
+
+  // const $home = $('.home .list #item')
+  const $modal = document.getElementById('modal');
+  const $overlay = document.getElementById('overlay');
+  const $hideModal = document.getElementById('hide-modal');
+
+  const $modalTitle = $modal.querySelector('h1');
+  const $modalImage = $modal.querySelector('img');
+  const $modalDescription = $modal.querySelector('p');
+})()
